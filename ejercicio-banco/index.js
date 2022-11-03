424e2f35a0337b42ece848bda9e2fdfcd742ce4e
mr-gantiva <ivaganva@gmail.com>
2022-11-03 15:30:10 -0300

    Creación del constructor para definir valores iniciales de saldo y numero - Condicional en depositos y cuentas para no dejar la cuenta en valores negativos

--- ejercicio-banco/index.js
+++ ejercicio-banco/index.js
@@ -5,11 +5,28 @@ class Cliente {
 
 class CuentaCorriente {
   numero;
-  saldo;
+  #saldo;
   agencia;
 
+  constructor() {
+    this.#saldo = 0;
+    this.numero = "";
+  }
+
   depositoEnCuenta(valor) {
-    this.saldo += valor;
+    if (valor > 0) {
+      this.#saldo += valor;
+    } else {
+      console.log("Valor a depositar no es valido");
+    }
+  }
+
+  retiroEnCuenta(valor) {
+    if (valor <= this.#saldo) {
+      this.#saldo -= valor;
+    } else {
+      console.log("Saldo insuficiente");
+    }
   }
 }
 
@@ -19,7 +36,6 @@ cliente1.rutCliente = "13804050";
 
 const CuentaCorriente1 = new CuentaCorriente();
 CuentaCorriente1.numero = "12365895";
-CuentaCorriente1.saldo = 1000;
 CuentaCorriente1.agencia = 1001;
 
 const cliente2 = new Cliente();
@@ -28,7 +44,6 @@ cliente2.rutCliente = "44599731";
 
 const CuentaCorriente2 = new CuentaCorriente();
 CuentaCorriente2.numero = "26588946";
-CuentaCorriente2.saldo = 3000;
 CuentaCorriente2.agencia = 1001;
 
 const cliente3 = new Cliente();
@@ -37,9 +52,14 @@ cliente3.rutCliente = "1689876";
 
 const CuentaCorriente3 = new CuentaCorriente();
 CuentaCorriente3.numero = "98947689";
-CuentaCorriente3.saldo = 4000;
 CuentaCorriente3.agencia = 1001;
 
-console.log("Saldo anterior: " + CuentaCorriente2.saldo);
+console.log("Saldo inicial: " + CuentaCorriente2.saldo);
 CuentaCorriente2.depositoEnCuenta(100);
-console.log("Saldo actual: " + CuentaCorriente2.saldo);
+console.log("Depositado: " + CuentaCorriente2.saldo);
+
+CuentaCorriente2.retiroEnCuenta(400);
+console.log("Saldo después del retiro : " + CuentaCorriente2.saldo);
+
+CuentaCorriente2.depositoEnCuenta(1000);
+console.log("Saldo después del deposito : " + CuentaCorriente2.saldo);
